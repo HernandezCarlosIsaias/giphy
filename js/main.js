eab6aeaea67361e11c1b7dd9c12f68e02d049aa0
HernandezCarlosIsaias <120617047+HernandezCarlosIsaias@users.noreply.github.com.>
2024-10-13 20:31:37 -0300

    termine de comentar el archivo js

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ const apiKey = "LbSB5LtJtC2OJev3E1fmUOtyaqfucypI"; //Api rest que nos otorga la 
 let entrada = document.getElementById("entrada"); // importamos la barra de busqueda desde el html
 let boton = document.getElementById("buscar");// importamos el boton de busquesa desde el html
 
-const contenedor = document.getElementById("muestra");
+const contenedor = document.getElementById("muestra");// importamos el contenedor del html donde se mostraran las imagenes
 console.log(entrada);
 
-const giphyFetch = (ak, kw) => {
+const giphyFetch = (ak, kw) => { // creamos la funcion giphyFech que recive dos valores y retorna una solicitud http
   return fetch(`
 https://api.giphy.com/v1/gifs/search
 ?api_key=${ak}
@@ -17,14 +17,14 @@ https://api.giphy.com/v1/gifs/search
 &bundle=messaging_non_clips`);
 };
 
-boton.addEventListener("click", () => {
-  term = entrada.value;
-  giphyFetch(apiKey, term)
-    .then((response) => {
+boton.addEventListener("click", () => { // añadimos el evento de escucha al boton cuando se le hace click
+  term = entrada.value; // guardamos en la variable term el texto ingresado en la barra de busqueda
+  giphyFetch(apiKey, term)// llamamos a la funcion de giphyFetch y le pasamos los valores apikey y term
+    .then((response) => {  // Esta promesa convierte la respuenta en formato json
       return response.json();
     })
 
-    .then((results) => {
+    .then((results) => { // esta promesa recorre el array de las imagenes y las inserta una por una en el contenedor con "Id=muestra"
       contenedor.innerHTML = "";
       results.data.forEach((element) => {
         console.log(element.images.original.url);
@@ -34,7 +34,7 @@ boton.addEventListener("click", () => {
       });
     })
 
-    .catch((error) => {
+    .catch((error) => { //en caso de que ocurra un error entra en el catch e imprime en la consola un mj de error
       console.error(error.message);
     });
 });
